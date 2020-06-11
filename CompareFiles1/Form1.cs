54fa9325d2d44111c28ec046c88a8d413a51bca9
mr-subroutine <darrylshack@outlook.com>
2020-06-11 11:41:55 -0700

    added functionality for file 1 and 2 buttons to cause file dialog to appear

--- CompareFiles1/Form1.cs
+++ CompareFiles1/Form1.cs
@@ -16,5 +16,33 @@ namespace CompareFiles1
         {
             InitializeComponent();
         }
+
+        string StartUpPath = Application.StartupPath;
+
+        protected string filename1 = "";
+        protected string filename2 = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "Select a text file";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt";
+            openFileDialog1.InitialDirectory = StartUpPath;
+            openFileDialog1.Title = "Open text file";
+
+            openFileDialog2.FileName = "Select a text file";
+            openFileDialog2.Filter = "Text files (*.txt)|*.txt";
+            openFileDialog2.InitialDirectory = StartUpPath;
+            openFileDialog2.Title = "Open text file";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult f1 = openFileDialog1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult f2 = openFileDialog1.ShowDialog();
+        }
     }
 }
